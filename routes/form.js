67795fd3b199a2b267f442cfe659be39523cbf47
Spryzu <spryzuofficial@gmail.com>
2023-07-19 11:24:36 -0600

    Express validator implemented

--- routes/form.js
+++ routes/form.js
@@ -1,12 +1,28 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
 const { createForm, callForm, getEntries } = require('../controllers/form');
+const { validateJson } = require('../middlewares/validateJson');
 
 const router = Router();
 
-router.post('/create', createForm);
+router.post('/create', [
+    check('name', 'Form name is required').not().isEmpty(),
+    check('fields', 'Fields are required').not().isEmpty(),
+    check('fields', 'Fields must be contained in an array').isArray(),
+    validateJson
+], createForm);
 
-router.post('/call', callForm);
+router.post('/call', [
+    check('token', 'Form token required').not().isEmpty(),
+    check('fields', 'Fields are required').not().isEmpty(),
+    check('fields', 'Fields must be contained in an array').isArray(),
+    validateJson
+], callForm);
 
-router.get('/entries', getEntries);
+router.get('/entries', [
+    check('token', 'Form token required').not().isEmpty(),
+    validateJson
+], getEntries);
 
 module.exports = router;
